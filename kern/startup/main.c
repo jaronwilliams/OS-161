656f46758fa59d7987237ba2e0c14e5e716a9a9f
jaron <jaronwilliams360@gmail.com>
2013-10-16 15:19:13 -0700

    Commit_2

--- kern/startup/main.c
+++ kern/startup/main.c
@@ -101,7 +101,7 @@ boot(void)
 	kprintf("%s", harvard_copyright);
 	kprintf("\n");
 
-	kprintf("Put-your-group-name-here's system version %s (%s #%d)\n", 
+	kprintf("Jaron Williams has edited the banner for Lab_2 %s (%s #%d)\n", 
 		GROUP_VERSION, buildconfig, buildversion);
 	kprintf("\n");
 
